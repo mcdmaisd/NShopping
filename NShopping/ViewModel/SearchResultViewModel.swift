f81de248aae9bb1b9b85948e5f59288ebaeee9d0
mcdmaisd <tkim0156@gmail.com>
2025-02-26 15:13:33 +0900

    refactor: 기존 subscribe를 bind로 변경

--- NShopping/ViewModel/SearchResultViewModel.swift
+++ NShopping/ViewModel/SearchResultViewModel.swift
@@ -14,47 +14,45 @@ class SearchResultViewModel {
     private var startIndex = UrlConstant.start
     var result: Shopping?
     var keyword: String?
-
+    
     struct Input {
         let filterButtonTapped: Observable<Int>
         let pagination: Observable<[IndexPath]>
     }
-
+    
     struct Output {
         let searchResult: BehaviorRelay<Shopping?>
         let selectedFilterButton: BehaviorRelay<Int>
         let scrollToTop: PublishRelay<Bool>
     }
-
+    
     func transform(input: Input) -> Output {
         let searchResult = BehaviorRelay<Shopping?>(value: result)
         let selectedFilterButton = BehaviorRelay<Int>(value: 0)
         let scrollToTop = PublishRelay<Bool>()
-
+        
         input.filterButtonTapped
             .distinctUntilChanged()
-            .subscribe(onNext: { [weak self] filterIndex in
-                guard let self = self else { return }
-                selectedFilterButton.accept(filterIndex)
-                self.startIndex = UrlConstant.start
-                self.search(filter: UrlConstant.sortingKeys[filterIndex], index: self.startIndex)
+            .bind(with: self, onNext: { owner, tag in
+                selectedFilterButton.accept(tag)
+                owner.startIndex = UrlConstant.start
+                owner.search(filter: UrlConstant.sortingKeys[tag], index: owner.startIndex)
                     .subscribe(onNext: { shopping in
                         searchResult.accept(shopping)
                         scrollToTop.accept(true)
                     }, onError: { error in
                         showAlert(error.localizedDescription)
                     })
-                    .disposed(by: self.disposeBag)
+                    .disposed(by: owner.disposeBag)
             })
             .disposed(by: disposeBag)
-
+        
         input.pagination
-            .subscribe(onNext: { [weak self] indexPaths in
-                guard let self = self else { return }
+            .bind(with: self, onNext: { owner, indexPaths in
                 guard let currentItems = searchResult.value?.items else { return }
                 if currentItems.count - 8 <= indexPaths.last?.row ?? 0 {
-                    self.startIndex += UrlConstant.display
-                    self.search(filter: UrlConstant.sortingKeys[selectedFilterButton.value], index: self.startIndex)
+                    owner.startIndex += UrlConstant.display
+                    owner.search(filter: UrlConstant.sortingKeys[selectedFilterButton.value], index: owner.startIndex)
                         .subscribe(onNext: { shopping in
                             var updatedShopping = searchResult.value
                             updatedShopping?.items.append(contentsOf: shopping.items)
@@ -63,16 +61,16 @@ class SearchResultViewModel {
                         }, onError: { error in
                             showAlert(error.localizedDescription)
                         })
-                        .disposed(by: self.disposeBag)
+                        .disposed(by: owner.disposeBag)
                 }
             })
             .disposed(by: disposeBag)
-
+        
         return Output(searchResult: searchResult,
                       selectedFilterButton: selectedFilterButton,
                       scrollToTop: scrollToTop)
     }
-
+    
     private func search(filter: String, index: Int) -> Observable<Shopping> {
         guard let keyword = keyword, !keyword.isEmpty else {
             return Observable.empty()
@@ -80,7 +78,7 @@ class SearchResultViewModel {
         let url = UrlComponent.Query.parameters(query: keyword, sort: filter, start: index).result
         return NetworkManager.shared.requestAPI(url)
     }
-
+    
     func setKeyword(_ keyword: String) {
         self.keyword = keyword
     }
